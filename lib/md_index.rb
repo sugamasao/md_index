1d738c23ab095e04ace3bb126353fdf43a5de209
sugamasao <sugamasao@gmail.com>
2013-12-08 14:38:54 +0900

    ヘルプ文言追加

--- lib/md_index.rb
+++ lib/md_index.rb
@@ -4,12 +4,20 @@ require 'md_index/parser'
 module MdIndex
   def self.run(file)
     begin
-      raise ArgumentError.new("file(#{file}) is not found") if file.nil? || !File.file?(file)
+      raise ArgumentError.new("file(#{file}) not found") if file.nil? || !File.file?(file)
       Parser.new(file).parse
     rescue => e
+      warn 'md_index Error.'
       warn e.message
+      puts
+      usage
       exit false
     end
+  end
+
+  def self.usage
+    puts 'usage: md_index markdown-format-file-path'
+    puts '       extract header level string.'
   end
 end
 
